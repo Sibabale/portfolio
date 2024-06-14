7cd2e26583a9e2a2996d2ee873db56f477eccb28
sibabale <sibabalejoja@gmail.com>
2024-06-14 11:10:28 +0200

    style: update the theme on the home page

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -3,16 +3,16 @@ import { Navbar } from "../../components/navbar";
 
 export const Home = () => {
   return (
-    <div className="bg-black h-screen">
+    <div className="h-screen">
       <Navbar />
 
       <main>
         <div className="lg:py-60 lg:px-20 sm:p-20  p-5">
           
           <div  className="flex flex-col">
-            <h2 className="text-white mb-10 text-4xl sm:text-8xl">hi</h2>
-            <p className="text-white font-thin text-2xl sm:text-5xl mb-5">my name is sibabale</p>
-            <p className="text-white font-thin text-2xl sm:text-5xl">i am a <span className="designer-text">designer</span> & <span className="developer-text">{`<developer>`}</span></p>
+            <h2 className="mb-10 text-4xl sm:text-8xl">hi</h2>
+            <p className="font-thin text-2xl sm:text-5xl mb-5">my name is sibabale</p>
+            <p className="font-thin text-2xl sm:text-5xl">i am a <span className="designer-text">designer</span> & <span className="developer-text">{`<developer>`}</span></p>
           </div>
 
         </div>
